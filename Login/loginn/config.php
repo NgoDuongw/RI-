1bc6eb91ceaed3ca222de1200e29184b3ae4da1c
ChiCong <congdhcpi00132@gmail.com>
2024-06-03 08:04:35 +0700

    up

--- Login/loginn/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Kết nối cơ sở dữ liệu
-$conn = mysqli_connect('localhost', 'root', '', 'user_db');
-
-// Kiểm tra kết nối
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-} 
-?>
