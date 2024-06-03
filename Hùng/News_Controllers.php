6ec3c1a3e982cf461c0e9bd506f69540971b332a
Hung <hungptpi00167@gmail.com>
2024-06-03 09:12:16 +0700

    up

---
+++ Hùng/News_Controllers.php
@@ -0,0 +1,9 @@
+<?php
+require_once '../database.php';
+require_once '../Models/News_Models.php';
+
+$database = new Database();
+$db = $database->getConnection();
+$news = new News($db);
+
+$newss = $news->getAllNews();
